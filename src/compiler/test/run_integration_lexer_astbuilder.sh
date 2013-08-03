82f69c28182690ac451ad7e6006bb71ada1c406c
Michael Gilliland <mjg.py3@gmail.com>
2013-08-03 11:15:20 -0400

    Writing more tests

--- src/compiler/test/run_integration_lexer_astbuilder.sh
+++ src/compiler/test/run_integration_lexer_astbuilder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-c++ test_integration_lexer_astbuilder.cpp ../../mitest/mitest.cpp ../../migrep/migrep.cpp ../lexer/foundtoken.cpp ../semparser/bfnodes.cpp ../semparser/astbuilder.cpp ../lexer/bflexer.cpp -I ../../mitest -I ../lexer -I ../../migrep -I ../semparser
+c++ test_integration_lexer_astbuilder.cpp ../syntaxchecker/bfnodechecker.cpp ../../mitest/mitest.cpp ../../migrep/migrep.cpp ../lexer/foundtoken.cpp ../semparser/bfnodes.cpp ../semparser/astbuilder.cpp ../lexer/bflexer.cpp -I ../../mitest -I ../lexer -I ../../migrep -I ../semparser -I ../syntaxchecker
 
 ./a.out
 rm a.out
--- src/compiler/test/test_integration_lexer_astbuilder.cpp
+++ src/compiler/test/test_integration_lexer_astbuilder.cpp
@@ -1,15 +1,48 @@
+#include <fstream>
+#include <string>
 #include "mitest.h"
 #include "bflexer.h"
 #include "astbuilder.h"
 #include "bfnodes.h"
+#include "bfnodechecker.h"
 using namespace std;
 
+string temp_file_name = "test_file.bf";
 
+void write_temp_bf_file(string text)
+{
+	ofstream fout;
+
+	fout.open(temp_file_name.c_str());
+	fout << text;
+	fout.close();
+}
+
+void the_ast_thats_built_for_hello_world_looks_correct(MiTester & tester, BfLexer lexer, AstBuilder builder)
+{
+	string code = "say \"Hello blowfish!\"\n";
+
+	write_temp_bf_file(code);
+        lexer.parseTokensFromFile(temp_file_name);
+        BfNode * ast = builder.buildAst(lexer.getTokens());
+
+	BfRoot * expected = new BfRoot();
+
+	(*expected).appendChild(new BfIdentifier("say"));
+	(*expected).appendChild(new BfString("\"Hello blowfish!\""));
+	(*expected).appendChild(new BfNewline());
+
+	tester.assertTrue(haveSameNodeStructure(expected, ast), "Tree - hello world");
+}
 
 
 int main()
 {
 	MiTester tester = MiTester();
+	BfLexer lex;
+	AstBuilder build;
+
+	the_ast_thats_built_for_hello_world_looks_correct(tester, lex, build);
 
 	tester.printResults();
 	return 0;
